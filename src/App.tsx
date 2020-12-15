0716abcc944b33e6d162bc8aaef484e7abb96ec0
Chris Tack <chris_tack@mac.com>
2020-12-14 20:36:46 -0800

    Implement CSS Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import './styles/App.scss';
 import BookSearch from './book-search/BookSearch';
+import SearchResults from './search-results/SearchResults';
+import ReadingWishList from './reading-wishlist/ReadingWishlist';
 
 function App() {
   return (
-      <div>
+      <div className="main">
         <header className="header">
           <div className="header--content">
             <h1>My Good Reads</h1>
           </div>
         </header>
-        <main>
-          <BookSearch/>
-        </main>
-
+        <div className="grid-container">
+          <div className="book-search"><BookSearch /></div>
+          <div className="search-results"><SearchResults /></div>
+          <div className="wishlist"><ReadingWishList /></div>
+        </div>
       </div>
   );
 }
